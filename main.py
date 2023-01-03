e525d13978bc13cfac8a85c42a00de1581b604f7
ferdyhape <ferdyhahan5@gmail.com>
2023-01-03 19:35:09 +0700

    provide a description of each process

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import pylightxl as xl
 
+# initiatie check datatype function
 def checkdatatype(datatype):
     if datatype.lower() == "int":
         return("integer")
@@ -14,7 +15,7 @@ def checkdatatype(datatype):
     else:
         return(datatype+" is not datatpye")
 
-# set tab title
+# set page config
 st.set_page_config(
     page_title="MySQL Generator | FERDYHAPE", 
     page_icon=":gear:",
@@ -27,6 +28,7 @@ st.title("""
 Generate Query Insert MySQL
 """)
 
+# sidebar for author introduction
 st.sidebar.markdown("""
 
 <head>
@@ -119,24 +121,17 @@ st.sidebar.markdown("""
 #initiate and refresh the list 
 atribute_datatype_list = []
 atribute_datatype_list.clear()
-
 column_address_list = []
 column_address_list.clear()
-
 atribute_name_list = []
 atribute_name_list.clear()
-
 atribute_datatype_list = []
 atribute_datatype_list.clear()
-dataype_list = ['int', 'str', 'date', 'char']
 
 # list of input
 table_name = st.text_input("Input table name ")
 input_atribute_name = st.text_input("Enter the Name of attribute sequentially (separated by comma, ex: id, name, address) ")
 input_atribute_datatype = st.text_input("Enter the data type of attribute sequentially (separated by comma, ex: int, str, str) ")
-# input_atribute_datatype = st.multiselect(
-#     'Enter the data type of attribute sequentially', dataype_list
-#     )
 input_column_address = st.text_input("Enter the address of the value column sequentially (separated by comma, ex: A, B, C) ")
 sheet_name = st.text_input("Sheet name ")
 range_rows = st.number_input("Range rows until ", 0)
@@ -167,45 +162,67 @@ if first_row_header:
     row_addr = 2
 
 #create generate_btn button
-
 generate_btn = st.button("Generate")
 
+#dump_address_list is a list to hold value addresses in excel
 dump_address_list = []
 
 # if generate_btn button clicked
 if generate_btn:
+    
+    # read the excel file that has been uploaded
+    excel_file = xl.readxl(excel_uploader)
+
+    #d isplays a preview of the table
+    d = {'Name': atribute_name_list, 'Datatype': atribute_datatype_list, 'Column Address': column_address_list}
+    df = pd.DataFrame(data=d)
     st.text("""
     Table Preview
     """)
-
-    excel_file = xl.readxl(excel_uploader)
-    d = {'Name': atribute_name_list, 'Datatype': atribute_datatype_list, 'Column Address': column_address_list}
-    df = pd.DataFrame(data=d)
     st.table(df)
 
+    #querycontent is a list that contains the value to be inserted per row
     querycontent = []
+
+    # For level 1 is used to retrieve the overall value from the Excel file that has been uploaded by row
+
+    # [i] is used to process row addresses in excel files
     for i in range (start_row, range_rows+1):
+
+        # [j] is used to process column addresses in excel files
         j = 0
         dump_address_list.clear()
-        
+
+        #For level 2 is used to retrieve the overall value from the Excel file that has been uploaded based on the column
         for col_address in column_address_list:
+
+            # the process of taking the value and put it in the variable "value"
             dump = column_address_list[j]+str(row_addr)
             value = str(excel_file.ws(ws=sheet_name).address(address=dump))
+            
+            # the process of checking whether the value is numeric, and then entering the dump_address_list list
             if value.isdigit():
                 dump_address_list.append(value)
             else:
                 dump_address_list.append(f'"{value}"')
+            
+            # j increment for move right column
             j+=1
+
+        # merge of value in one row, then entering querycontent list 
         query = ', '.join(dump_address_list)
         querycontent.append(f"({query})")
+
+        # row_addr increment for move bottom row
         row_addr+=1
-        
+    
+    # final_query is the final of all that has been set to display format to the user
     final_query = ',\n'.join(querycontent)
     
+    #displays queries
     st.text("""
     Query
     """)
-
     st.markdown(f"""
     ```shell
     INSERT INTO {table_name} ({name_for_query}) \nVALUES \n{final_query};
